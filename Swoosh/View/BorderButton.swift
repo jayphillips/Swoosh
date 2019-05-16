05207ec7ba3c595449d7346b2ef446b15b95a9d7
Jay Phillilps <jayphillips@icloud.com>
2019-05-16 17:05:25 -0400

    first commit

---
+++ Swoosh/View/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  Swoosh
+//
+//  Created by Jay Phillips on 5/16/19.
+//  Copyright © 2019 Jay Phillips. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
